60fd70991e3655af224007bdbeb3ab1b5a3ce612
Zoran . Velichkovski <z.trendo@gmail.com>
2018-10-06 11:46:56 +0300

    ---test

---
+++ cmd/go-sofia/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/rumyantseva/go-sofia/internal/diagnostics"
+)
+
+func main() {
+	log.Print("Hello, World")
+
+	router := mux.NewRouter()
+	router.HandleFunc("/", hello)
+
+	go func() {
+		err := http.ListenAndServe(":8080", router)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	diagnostics := diagnostics.NewDiagnostics()
+	err := http.ListenAndServe(":8585", diagnostics)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, http.StatusText(http.StatusOK))
+}
